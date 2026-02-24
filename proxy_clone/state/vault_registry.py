f045f8dbbe4064c9c7b0a5e34b97f46b7291fb3c
rezami <reza.mirzaeifard@gmail.com>
2026-02-24 23:06:55 +0100

    Tighten vault registry typing

--- proxy_clone/state/vault_registry.py
+++ proxy_clone/state/vault_registry.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import secrets
 import threading
-from collections.abc import Callable
+from collections.abc import Callable, MutableMapping
 from typing import Any
 
 from .credential_vault import CredentialVault
@@ -18,7 +18,7 @@ class VaultRegistry:
     def vaults(self) -> dict[str, CredentialVault]:
         return self._vaults
 
-    def current(self, session_store: Any) -> CredentialVault:
+    def current(self, session_store: MutableMapping[str, Any]) -> CredentialVault:
         vault_id = session_store.get("vault_id")
         if not vault_id:
             vault_id = secrets.token_urlsafe(16)
@@ -31,10 +31,9 @@ class VaultRegistry:
                 self._vaults[vault_id] = instance
             return instance
 
-    def drop_current(self, session_store: Any) -> None:
+    def drop_current(self, session_store: MutableMapping[str, Any]) -> None:
         vault_id = session_store.pop("vault_id", None)
         if not vault_id:
             return
         with self._lock:
             self._vaults.pop(vault_id, None)
-
